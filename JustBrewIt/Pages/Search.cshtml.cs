1df3abc48b0d05b8792075cf906ef9fd29c551f5
PratibhaSingh16 <77543839+PratibhaSingh16@users.noreply.github.com>
2021-03-26 17:17:02 -0400

    brewery api call

--- JustBrewIt/Pages/Search.cshtml.cs
+++ JustBrewIt/Pages/Search.cshtml.cs
@@ -1,16 +1,26 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using QuickType;
 
 namespace JustBrewIt.Pages
 {
     public class SearchModel : PageModel
     {
+        public object MyBreweryAPI { get; private set; }
+
         public void OnGet()
         {
+            using (var webClient = new WebClient())
+            {
+                var jsonString = webClient.DownloadString("https://api.openbrewerydb.org/breweries?by_city=cincinnati&brewery_type=regional");
+                Welcome[] welcome = Welcome.FromJson(jsonString);
+                ViewData["MyBreweryAPI"] = MyBreweryAPI;
+            }
         }
     }
 }
