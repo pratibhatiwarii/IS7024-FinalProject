ed9fb9e06e7231a057833c089f292ba95c4f3e24
shashi <47906013+snayal@users.noreply.github.com>
2021-03-30 00:16:12 -0400

    GoogleAPI text file is missing

--- JustBrewIt/Pages/ViewDetails.cshtml.cs
+++ JustBrewIt/Pages/ViewDetails.cshtml.cs
@@ -19,6 +19,7 @@ namespace JustBrewIt.Pages
             using (var webClient = new WebClient())
             {
                 string key = System.IO.File.ReadAllText("GoogleAPIKey.txt");
+                string googleString = webClient.DownloadString("https://api.openbrewerydb.org/breweries?by_city=cincinnati&brewery_type=regional"+key);
                 string fields = "&fields=business_status,formatted_address,geometry,icon,name,photos,place_id,plus_code,types,price_level,rating,user_ratings_total&key=";
                 string api = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=";
                 string input = "Mad Tree Brewing";
